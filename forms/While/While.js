94a7b81e21e147707ec08d316e909b16e38d4577
Laura Higueras <lhi72634@creighton.edu>
2021-03-02 21:05:55 -0600

    Update While.js

--- forms/While/While.js
+++ forms/While/While.js
@@ -1,12 +1,3 @@
-/*
-First program - create a program that uses a 'while' loop and outputs each iteration as shown: 
-> scenario:
-form named 'while'
-output to console: 
-This is loop iteration 1.
-This is loop iteration 2.
-etc....
-*/
 let iteration = ['1', '2', '3'];
  
 for (let i = 0; i < iteration.length; i++)
