8b98fcac58fe76efa2e22392e144e29e8d09e09b
Laura Higueras <lhi72634@creighton.edu>
2021-03-03 16:34:56 -0600

    Update While.js

--- forms/While/While.js
+++ forms/While/While.js
@@ -1,4 +1,7 @@
 let iteration = ['1', '2', '3'];
- 
-for (let i = 0; i < iteration.length; i++)
+let i = 0
+
+while (i < iteration.length) {
     console.log(`This is loop iteration ${iteration[i]}`)
+    i++
+}
