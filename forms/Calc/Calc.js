edda9d7900ffaf772997b6b31fbbb48acf2b0ce6
Laura Higueras <lhi72634@creighton.edu>
2021-03-02 22:13:38 -0600

    all sections

---
+++ forms/Calc/Calc.js
@@ -0,0 +1,19 @@
+/*
+Third program - create a program that uses a function to add two numbers from the user and return their sum to the main (calling) program.  
+> scenario:
+form named ‘calc’
+function named ‘Calculate’ that takes 2 parameters named 'num1' and 'num2' and returns their sum to the main program
+in the main program, gets 2 numbers from user and send them to the function
+Note: remember to catch the answer that the function returns back to the main program
+in the main program, outputs to console in this format:
+        The sum of X and Y is Z.
+
+let num1 = prompt("Introduce first number")
+let num2 = prompt("Introduce second number")
+let summedNumbers = sum(num1, num2)
+function calculate(num1, num2) {
+  return num1 + num2;
+  };
+  console.log(`The sum of ${num1} and ${num2} is ${summedNumbers}`)
+  
+  */
