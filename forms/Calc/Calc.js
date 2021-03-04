36ffe28e7f8d3791c70bc9db644bdb73e741c51d
Laura Higueras <lhi72634@creighton.edu>
2021-03-03 19:49:09 -0600

    Changes P2

---
+++ forms/Calc/Calc.js
@@ -0,0 +1,11 @@
+let num1 = Number(prompt("Introduce first number"))
+let num2 = Number(prompt("Introduce second number"))
+
+function sumNumbers (num1, num2) {
+    number = Number(num1 + num2)
+    let answer = number
+    return answer
+     }
+     let answer = sumNumbers (num1, num2)
+console.log(`The sum of ${num1} and ${num2} is ${answer}`)
+    
